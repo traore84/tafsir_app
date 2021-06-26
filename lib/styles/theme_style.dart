666eb6aa96ffbe1dbf2daa2a4761cdf95b9f4162
traore <trabasse84@gmail.com>
2021-06-26 01:01:24 +0000

    theme style
    creation de costom theme

---
+++ lib/styles/theme_style.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:tafsir_app/styles/color_style.dart';
+
+final customTheme = ThemeData.light().copyWith(
+  scaffoldBackgroundColor: Colors.white,
+  primaryColor: primaryColor,
+  
+);
