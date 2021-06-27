f942f5a9e2eb67de77c54d529fb80ed6fde63d17
traore <trabasse84@gmail.com>
2021-06-27 13:11:25 +0000

    player controls
    fin de creation de player controls

--- lib/screens/player_screen.dart
+++ lib/screens/player_screen.dart
@@ -39,35 +39,33 @@ class PlayerScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Obx(
-        () => Stack(
-          children: [
-            Positioned(
-              left: 0,
-              right: 0,
-              height: Get.height * .2,
-              child: Container(
-                color: primaryColor,
-              ),
+      body: Stack(
+        children: [
+          Positioned(
+            left: 0,
+            right: 0,
+            height: Get.height * .2,
+            child: Container(
+              color: primaryColor,
             ),
-            Positioned(
-              right: 0,
-              left: 0,
-              top: Get.height * .16,
-              height: Get.height - Get.height * .16,
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(30),
-                  ),
+          ),
+          Positioned(
+            right: 0,
+            left: 0,
+            top: Get.height * .16,
+            height: Get.height - Get.height * .16,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
                 ),
-                child: playerBody(),
               ),
+              child: playerBody(),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -88,16 +86,78 @@ class PlayerScreen extends StatelessWidget {
                   style: playerBodyTextStyle,
                 ),
               ),
-              slider()
+              slider(),
+              playerControls(),
             ],
           ),
         ),
+        playerBottomControls(),
       ],
     );
   }
 
+  Widget playerBottomControls() {
+    return Container(
+      child: Row(
+        children: [
+          InkWell(
+            child: Container(
+              child: Icon(
+                Icons.repeat,
+                color: Colors.black,
+                size: 50,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget playerControls() {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          InkWell(
+            child: Container(
+              child: Icon(
+                Icons.skip_previous,
+                color: Colors.black,
+                size: 30,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          InkWell(
+            child: Container(
+              child: Icon(
+                Icons.play_arrow,
+                color: primaryColor,
+                size: 60,
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          InkWell(
+            child: Container(
+              child: Icon(
+                Icons.skip_next,
+                color: Colors.black,
+                size: 30,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
   Widget slider() {
-    double value = playerController.sliderValue.value;
     return Container(
       width: Get.width,
       child: SliderTheme(
